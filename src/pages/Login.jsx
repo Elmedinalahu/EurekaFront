26af3893203c8d7456742d1c9ff4b8c975642401
erblina.zhegrova <erblina.zhegrova@tretek.io>
2024-07-14 20:49:20 +0200

    Added js and css to Login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,40 +1,33 @@
-import './Login.css'
+import React from 'react';
+import './Login.css';
 
 export default function Login() {
   return (
     <div className="login">
-      <div className="rectangle-1">
-      </div>
-      <div className="rectangle-39">
-      </div>
-      <div className="welcome-to-the-next-level">
-      Welcome to the next level.
-      </div>
-      <div className="container-2">
-        <span className="email">
-        Email
-        </span>
-      </div>
-      <div className="container-1">
-        <span className="password">
-        Password
-        </span>
-      </div>
-      <div className="container">
-        <div className="rectangle-38">
+      <div className="rectangle"></div>
+      <div className="content">
+        <div className="welcome-to-the-next-level">
+          Welcome to <br />
+          the next level!
         </div>
-        <p className="keep-me-logged-in-forgot-password">
-        <span className="keep-me-logged-in-forgot-password-sub-14"></span><span className="keep-me-logged-in-forgot-password-sub-13"></span><span></span>
+        <div className="container-1">
+          <span className="email">Email</span>
+        </div>
+        <div className="container-2">
+          <span className="password">Password</span> {/* Text "Password" here */}
+        </div>
+        <div className="container">
+          <div className="rectangle-38"></div> {/* Small box */}
+          <p className="keep-me-logged-in">Keep me logged in</p>
+          <p className="forgot-password">Forgot Password ?</p>
+        </div>
+        <div className="container-3">
+          <span className="sign-in">Sign in</span>
+        </div>
+        <p className="dont-have-an-account">
+          Don't have an account? <span className="register">Register</span>
         </p>
       </div>
-      <div className="container-3">
-        <span className="sign-in">
-        Sign in
-        </span>
-      </div>
-      <p className="dont-have-an-account-register">
-      <span className="dont-have-an-account-register-sub-16"></span><span className="dont-have-an-account-register-sub-15"></span><span></span>
-      </p>
     </div>
-  )
+  );
 }
